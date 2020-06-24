93119402dfe4bbded87df36961659eb993b201eb
Pierre Tessier <pierre@pierretessier.com>
2020-06-24 09:13:53 -0400

    default settings for db conn

--- services/config/config.go
+++ services/config/config.go
@@ -30,14 +30,14 @@ var (
 
 	// MySQLGetDelay is how long retrieving a customer record takes.
 	// Using large value mostly because I cannot click the button fast enough to cause a queue.
-	MySQLGetDelay = 300 * time.Millisecond
+	MySQLGetDelay = 400 * time.Millisecond
 
 	// MySQLGetDelayStdDev is standard deviation
 	MySQLGetDelayStdDev = MySQLGetDelay / 10
 
 	// MySQLMutexDisabled controls whether there is a mutex guarding db query execution.
 	// When not disabled it simulates a misconfigured connection pool of size 1.
-	MySQLMutexDisabled = false
+	MySQLMutexDisabled = true
 
 	// MySQLSlowCustomerDelay controls the MySQL Delay to apply to select "slow" customers
 	MySQLSlowCustomerDelay = MySQLGetDelay * 5
